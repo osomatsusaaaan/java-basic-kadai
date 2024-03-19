bea3a8209e8b9aeadc59d284faef4013d914c422
osomatsusaaaan <154360132+osomatsusaaaan@users.noreply.github.com>
2024-03-19 18:46:21 +0900

    課題＿０１８　提出

---
+++ kadai_018/KatoExec_Chapter18.java
@@ -0,0 +1,20 @@
+package kadai_018;
+
+public class KatoExec_Chapter18 {
+	public static void main(String[] args) {
+		
+		//各クラスのオブジェクト
+		KatoTaro_Chapter18 KatoTaro = new KatoTaro_Chapter18();
+		KatoTaro.setGivenName();
+		KatoTaro.execIntroduce();
+		
+		KatoIchiro_Chapter18 KatoIchiro = new KatoIchiro_Chapter18();
+		KatoIchiro.setGivenName();
+		KatoIchiro.execIntroduce();
+		
+		KatoHanako_Chapter18 KatoHanako = new KatoHanako_Chapter18();
+		KatoHanako.setGivenName();
+		KatoHanako.execIntroduce();
+		
+	}
+}
---
+++ kadai_018/KatoTaro_Chapter18.java
@@ -0,0 +1,12 @@
+package kadai_018;
+
+public class KatoTaro_Chapter18 extends Kato_Chapter18{
+	public void setGivenName() {
+		givenName = "太郎";
+		System.out.println("名前は" + famiryName + givenName + "です");
+	}
+	
+	public void eachIntroduce() {
+		System.out.println("Javaが得意です");
+	}
+}
