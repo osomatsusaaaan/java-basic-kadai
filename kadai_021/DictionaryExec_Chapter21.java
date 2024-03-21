4a0c090cca5c4fd075a3451c36bf3af7683035f0
osomatsusaaaan <154360132+osomatsusaaaan@users.noreply.github.com>
2024-03-22 00:40:11 +0900

    課題＿０２１　提出

---
+++ kadai_021/DictionaryExec_Chapter21.java
@@ -0,0 +1,15 @@
+package kadai_021;
+
+public class DictionaryExec_Chapter21 {
+
+	public static void main(String[] args) {
+		
+		Dictionary_Chapter21 Dictionary = new Dictionary_Chapter21();
+		
+		String[] word = {"apple","banana","grape","orange"};
+		
+		
+		Dictionary.search(word);
+	}
+
+}
